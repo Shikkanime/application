82c8b95573ae63dc521936b2ec1d856405e5049c
Ziedelth <watteaupierre@gmail.com>
2025-02-19 23:10:11 +0100

    improve member image rendering logic and default state handling

--- lib/components/member_image.dart
+++ lib/components/member_image.dart
@@ -18,38 +18,51 @@ class MemberImage extends StatelessWidget {
   final bool hasBorder;
 
   @override
-  Widget build(final BuildContext context) => Container(
-        decoration: hasBorder
-            ? BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.fromBorderSide(
-                  BorderSide(
-                    color: Theme.of(context).textTheme.titleLarge!.color!,
-                    width: 2,
-                  ),
+  Widget build(final BuildContext context) {
+    final DecoratedBox defaultMemberImage = DecoratedBox(
+      decoration: BoxDecoration(color: Theme.of(context).canvasColor),
+      child: const Icon(
+        Icons.person,
+        size: 24,
+      ),
+    );
+
+    return Container(
+      decoration: hasBorder
+          ? BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.fromBorderSide(
+                BorderSide(
+                  color: Theme.of(context).textTheme.titleLarge!.color!,
+                  width: 2,
                 ),
-              )
-            : null,
-        padding: hasBorder ? const EdgeInsets.all(1) : null,
-        child: ImageComponent(
-          uuid: true == member?.hasProfilePicture ? (member?.uuid ?? '') : '',
-          version: MemberController.instance.imageVersion.toString(),
-          borderRadius: const BorderRadius.all(Radius.circular(360)),
-          width: hasBorder && width != null ? width! - 6 : width,
-          height: hasBorder && height != null ? height! - 6 : height,
-          placeholder: const Padding(
-            padding: EdgeInsets.all(24),
-            child: CircularProgressIndicator(
-              strokeWidth: 2,
+              ),
+            )
+          : null,
+      padding: hasBorder ? const EdgeInsets.all(1) : null,
+      child: member!.hasProfilePicture
+          ? ImageComponent(
+              uuid: member!.uuid,
+              version: MemberController.instance.imageVersion.toString(),
+              borderRadius: const BorderRadius.all(Radius.circular(360)),
+              width: hasBorder && width != null ? width! - 6 : width,
+              height: hasBorder && height != null ? height! - 6 : height,
+              placeholder: const Padding(
+                padding: EdgeInsets.all(24),
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                ),
+              ),
+              errorWidget: defaultMemberImage,
+            )
+          : ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(360)),
+              child: SizedBox(
+                width: hasBorder && width != null ? width! - 6 : width,
+                height: hasBorder && height != null ? height! - 6 : height,
+                child: defaultMemberImage,
+              ),
             ),
-          ),
-          errorWidget: DecoratedBox(
-            decoration: BoxDecoration(color: Theme.of(context).canvasColor),
-            child: const Icon(
-              Icons.person,
-              size: 32,
-            ),
-          ),
-        ),
-      );
+    );
+  }
 }
